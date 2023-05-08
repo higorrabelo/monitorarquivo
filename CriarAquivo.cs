5f73260d1a5cda82636859caf0d2b871bb2f9b71
Higor <suporte1@brasolucoes.com>
2023-05-08 15:30:11 -0300

    Primeiro commit

---
+++ CriarAquivo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileChecker {
+    internal class CriarAquivo {
+        public static void criarArquivo() {
+            try {
+                string path = Directory.GetCurrentDirectory();
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine(conteudoArquivo());
+                sw.Close();
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static string conteudoArquivo() { 
+            string conteudo = null;
+            conteudo = "Conteudo do arquivo não modificado";
+            return conteudo;
+        }
+        public static string lerArquivo() {
+            StreamReader sr = new StreamReader(Directory.GetCurrentDirectory());
+            string linha = sr.ReadLine();  
+            while(linha != "") {
+                Console.WriteLine(linha);
+                linha = sr.ReadLine();
+            }
+            sr.Close();
+            return linha;
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileChecker {
+    internal class Program {
+
+        private static FileSystemWatcher _monitorar;
+
+        public static void MonitorarArquivos(string path, string filtro) {
+            try {
+                _monitorar = new FileSystemWatcher(path, filtro) {
+                    IncludeSubdirectories = false
+                };
+                _monitorar.Created += OnFileChanged;
+                _monitorar.Changed += OnFileChanged;
+                _monitorar.Deleted += OnFileChanged;
+                _monitorar.Renamed += OnFileRenamed;
+
+                _monitorar.EnableRaisingEvents = true;
+
+
+                Console.WriteLine($"Monitorando arquivos e: {filtro}");
+            }
+            catch(ArgumentException ag) {
+                Console.WriteLine(ag.Message);
+            }
+
+        }
+        private static void OnFileChanged(Object sender, FileSystemEventArgs e) {
+            Console.WriteLine($"O arquivo {e.Name} {e.ChangeType}");
+        }
+        private static void OnFileRenamed(object sender, RenamedEventArgs e) {
+            Console.WriteLine($"O arquivo {e.OldName}{e.ChangeType} para {e.Name}");
+        }
+        static void Main(string[] args) {
+            Console.WriteLine("Monitorando Arquivo com: FileSystemWatcher");
+            string path = Directory.GetCurrentDirectory();
+            string filtro = "teste.txt";
+            MonitorarArquivos(path, filtro);
+            Console.ReadLine();
+        }
+    }
+}
